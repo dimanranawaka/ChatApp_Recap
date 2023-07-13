296388135e9a946c8d7d3e41e648d58483ab0910
dimanranawaka <dimanranawaka@gmail.com>
2023-07-13 19:30:13 +0530

    Configuring ClientController According to Client.fxml

--- src/lk/ijse/Live_Chat_Application/controller/ClientController.java
+++ src/lk/ijse/Live_Chat_Application/controller/ClientController.java
@@ -1,4 +1,40 @@
 package lk.ijse.Live_Chat_Application.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
 public class ClientController {
+    public TextField txtMessage;
+    public ScrollPane txtArea;
+    public VBox messageArea;
+    public Label txtClientName;
+    public AnchorPane emojiPane;
+    public ImageView emoji1;
+    public ImageView emoji6;
+    public ImageView emoji2;
+    public ImageView emoji7;
+    public ImageView emoji3;
+    public ImageView emoji8;
+    public ImageView emoji4;
+    public ImageView emoji9;
+    public ImageView emoji5;
+    public ImageView emoji10;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
+
+    public void emojiOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void openFileChoser(MouseEvent mouseEvent) {
+    }
+
+    public void sendEmojiOnAction(MouseEvent mouseEvent) {
+    }
 }
