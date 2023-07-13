ca576e6025648fbc8de0054a64470a35c16f5037
dimanranawaka <dimanranawaka@gmail.com>
2023-07-13 19:48:09 +0530

    Configuring Login Class

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,9 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Login extends Application {
 
@@ -8,7 +12,9 @@ public class Login extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("lk/ijse/Live_Chat_Application/controller/LoginController.java"))));
+        primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 }
