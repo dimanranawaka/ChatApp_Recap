a03f4ffc494b9cefc607b12d3cf1749db0971abf
dimanranawaka <dimanranawaka@gmail.com>
2023-07-13 16:44:17 +0530

    Configuring LoginForm and LoginController

--- src/lk/ijse/Live_Chat_Application/controller/LoginController.java
+++ src/lk/ijse/Live_Chat_Application/controller/LoginController.java
@@ -1,4 +1,15 @@
 package lk.ijse.Live_Chat_Application.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginController {
+    public AnchorPane loginPane;
+    public TextField txtUserName;
+    public Button btnSend;
+
+    public void loadClientPageOnAction(ActionEvent actionEvent) {
+    }
 }
