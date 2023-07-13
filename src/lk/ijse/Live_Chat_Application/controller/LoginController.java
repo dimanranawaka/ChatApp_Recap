156aac2a9e82419b16bac7cb31d352035926fa80
dimanranawaka <dimanranawaka@gmail.com>
2023-07-13 19:39:52 +0530

    Configuring LoginController

--- src/lk/ijse/Live_Chat_Application/controller/LoginController.java
+++ src/lk/ijse/Live_Chat_Application/controller/LoginController.java
@@ -1,15 +1,30 @@
 package lk.ijse.Live_Chat_Application.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class LoginController {
     public AnchorPane loginPane;
     public TextField txtUserName;
     public Button btnSend;
+    public static String clientName;
 
-    public void loadClientPageOnAction(ActionEvent actionEvent) {
+    public void loadClientPageOnAction(ActionEvent actionEvent) throws IOException {
+        clientName = txtUserName.getText();
+
+        URL resource = getClass().getResource("/lk/ijse/Live_Chat_Application/view/Client.fxml");
+
+        Parent load = FXMLLoader.load(resource);
+
+        loginPane.getChildren().clear();
+
+        loginPane.getChildren().add(load);
     }
 }
